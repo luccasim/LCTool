77a3caa7ec58135d452cb27dcb5409e9689d9947
Luc CASIMIR <casimir.luc@gmail.com>
2024-01-31 00:24:04 +0100

    Work on snackBar

--- Sources/LCTool/SnackBar.swift
+++ Sources/LCTool/SnackBar.swift
@@ -7,85 +7,118 @@
 
 import SwiftUI
 
-struct Test: View {
+public struct SnackBarInfo {
+    let message: String
+    let image: Image?
+    var backgroundColor: Color = .gray
+}
+
+// MARK: - Views
+
+private struct SnackBarInfoViewModifier: ViewModifier {
     
-    @State private var isAppear = true
-    @State private var label = "Button"
+    @State private var info: SnackBarInfo = .init(message: "", image: nil)
+    @State private var isAppear = false
     
     var offset: CGFloat {
         isAppear ? 0 : 100.0
     }
     
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.orange)
-                .ignoresSafeArea()
-            actionButton
-        }
-        .onTapGesture {
-            isAppear = false
-        }
-        .overlay(alignment: .bottom) {
-            snackbar
-        }
-    }
-    
-    var actionButton: some View {
-        Button(action: {
-            CANotificationManager.shared.post(id: "test", value: "Boobs")
-        }, label: {
-            Text("push me")
-        })
-    }
-    
-    var snackbar: some View {
-        SnackBarInfoView(message: "Bonjour TOTO", image: .init(systemName: "doc"))
-            .offset(y: offset)
-            .animation(.spring, value: offset)
-            .onReceive(NotificationCenter.default.publisher(for: .init("test")), perform: { output in
-                if let output = output.object as? String {
-                    isAppear = true
+    func body(content: Content) -> some View {
+        content
+            .overlay(alignment: .bottom) {
+                SnackBarView(info: info)
+                    .offset(y: offset)
+                    .animation(.spring, value: offset)
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .init("LCTool.SnackBarInfoViewModifier"))) { output in
+                if let info = output.object as? SnackBarInfo {
+                    self.info = info
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        self.isAppear = true
+                    }
                     DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                         self.isAppear = false
                     }
                 }
-            })
+            }
     }
 }
 
-struct SnackBarInfoView: View {
+private struct SnackBarView: View {
     
-    let message: String
-    let image: Image
-    
-    // MARK: - Body
-    
+    let info: SnackBarInfo
+        
     var body: some View {
         HStack {
-            image
-                .renderingMode(.template)
-                .resizable()
-                .foregroundColor(.white)
-                .frame(width: 16, height: 16)
+            if let image = info.image {
+                image
+                    .renderingMode(.template)
+                    .foregroundColor(.white)
+                    .frame(width: 12, height: 12)
+            }
             
-            Text(message)
+            Text(info.message)
                 .font(.caption)
+                .fontWeight(.semibold)
                 .foregroundStyle(Color.white)
+            
+            Spacer()
         }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(RoundedRectangle(cornerRadius: 8).fill(info.backgroundColor))
         .padding(.horizontal)
-        .background(background)
+    }
+}
+
+// MARK: - Extensions
+
+public extension NotificationCenter {
+    static func snackBarNotif(info: SnackBarInfo) {
+        NotificationCenter.default.post(name: .init("LCTool.SnackBarInfoViewModifier"), object: info)
+    }
+}
+
+public extension View {
+    func snackBarCenter() -> some View {
+        modifier(SnackBarInfoViewModifier())
+    }
+}
+
+// MARK: - Preview
+
+struct SnackBarPreview: View {
+        
+    let info = SnackBarInfo(message: "Bonjour je suis un dracofeu, j'aime les tomates et je crache du feu",
+                                 image: nil,
+                                 backgroundColor: .red)
+    let info2 = SnackBarInfo(message: "Bonjour je suis un Florizarre, j'aime les salade et fouette les arabes",
+                                  image: .init(systemName: "doc"),
+                                 backgroundColor: .green)
+    let info3 = SnackBarInfo(message: "Bonjour je suis un Tortank, j'aime les raisins et j'arose!",
+                                 image: nil,
+                                 backgroundColor: .blue)
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .ignoresSafeArea()
+            actionButton
+        }
+        .snackBarCenter()
     }
     
-    // MARK: - Methods
-    
-    private var background: some View {
-        RoundedRectangle(cornerRadius: 16)
-            .fill(Color.gray)
-            .frame(height: 24)
+    var actionButton: some View {
+        Button(action: {
+            NotificationCenter.snackBarNotif(info: [info, info2, info3].randomElement()!)
+        }, label: {
+            Text("try me")
+        })
     }
 }
 
 #Preview {
-    Test()
+    SnackBarPreview()
 }
