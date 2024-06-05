d8cf5d0ee2cb8fb89b49d4dd99c30ededd9591df
lcasimir <lcasimir@corp.free.fr>
2024-06-05 17:29:58 +0200

    update snackBar

--- Sources/LCTool/SnackBar.swift
+++ Sources/LCTool/SnackBar.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import Combine
 
+// MARK: - Model
+
 public struct SnackBarInfo {
     
     public init(message: String) {
@@ -39,6 +41,10 @@ public struct SnackBarInfo {
     public enum Position {
         case top, bottom
     }
+    
+    public enum Style {
+        case one, stack
+    }
 }
 
 // MARK: - Extensions
@@ -50,51 +56,76 @@ public extension NotificationCenter {
 }
 
 public extension View {
-    func snackBarCenter(position: SnackBarInfo.Position = .bottom) -> some View {
-        modifier(SnackBarInfoViewModifier(position: position))
+    func snackBarCenter(position: SnackBarInfo.Position = .bottom, 
+                        style: SnackBarInfo.Style = .one) -> some View {
+        modifier(SnackBarInfoViewModifier(position: position, style: style))
     }
 }
 
-// MARK: - Views
+// MARK: - ViewModel
 
 private class SnackBarVM: ObservableObject {
     
     @Published var info: SnackBarInfo = .init(message: "", image: nil)
+    @Published var infos = [OrderedSnackInfo]()
     @Published var isAppear = false
+    @Published var isPresentedDetail = false
+
+    private var timer: Timer?
     
-    let subject = PassthroughSubject<SnackBarInfo, Never>()
-    var cancellable: AnyCancellable?
+    struct OrderedSnackInfo: Identifiable {
+        var id: Int
+        var info: SnackBarInfo
+    }
     
-    init() {
-         cancellable = subject
-            .throttle(for: .seconds(5), scheduler: RunLoop.main, latest: true)
-            .sink { info in
-                self.update(info: info)
-            }
+    var recentInfo: [OrderedSnackInfo] {
+        infos.suffix(3)
     }
     
     func add(info: SnackBarInfo) {
-        subject.send(info)
+        update(info: info)
+        if !isPresentedDetail {
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(withTimeInterval: info.displayTime, repeats: false, block: { timer in
+                self.clearToast()
+            })
+        }
+    }
+    
+    private func clearToast() {
+        self.isAppear = false
+        self.infos = []
+        self.isPresentedDetail = false
+    }
+    
+    func openToasts() {
+        timer?.invalidate()
+        self.isPresentedDetail = true
+    }
+    
+    func closeToasts() {
+        self.clearToast()
     }
     
     private func update(info: SnackBarInfo) {
-        self.info = info
+        infos.append(.init(id: infos.count, info: info))
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
             self.isAppear = true
             if let haptic = info.haptic {
                 UIImpactFeedbackGenerator(style: haptic).impactOccurred()
             }
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + info.displayTime) {
-            self.isAppear = false
-        }
     }
 }
+
+// MARK: - ViewModifier
 
 private struct SnackBarInfoViewModifier: ViewModifier {
     
     @StateObject private var viewModel = SnackBarVM()
+    
     let position: SnackBarInfo.Position
+    let style: SnackBarInfo.Style
     
     var offset: CGFloat {
         viewModel.isAppear ? 
@@ -108,9 +139,7 @@ private struct SnackBarInfoViewModifier: ViewModifier {
             content
         }
         .overlay(alignment: position == .bottom ? .bottom : .top) {
-            SnackBarView(info: viewModel.info)
-                .offset(y: offset)
-                .animation(.spring, value: offset)
+            snackBarContent
         }
         .onReceive(NotificationCenter.default.publisher(for: .init("LCTool.SnackBarInfoViewModifier"))) { output in
             if let info = output.object as? SnackBarInfo {
@@ -118,7 +147,23 @@ private struct SnackBarInfoViewModifier: ViewModifier {
             }
         }
     }
+    
+    @ViewBuilder
+    private var snackBarContent: some View {
+        switch style {
+        case .one:
+            SnackBarView(info: viewModel.info)
+                .offset(y: offset)
+                .animation(.spring, value: offset)
+        case .stack:
+            SnackBarStack(viewModel: viewModel, position: position)
+                .offset(y: offset)
+                .animation(.spring, value: offset)
+        }
+    }
 }
+
+// MARK: - Views
 
 private struct SnackBarView: View {
     
@@ -148,36 +193,92 @@ private struct SnackBarView: View {
                 .opacity(0.9)
         )
         .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 6)
-        .padding(.horizontal)
+        .padding(.horizontal, 24)
     }
 }
 
+private struct SnackBarStack: View {
+    
+    @ObservedObject var viewModel: SnackBarVM
+    let position: SnackBarInfo.Position
+    
+    var body: some View {
+        ZStack {
+            if viewModel.isPresentedDetail {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.closeToasts()
+                        } label: {
+                            Circle()
+                                .foregroundStyle(Color.gray.opacity(0.5))
+                                .frame(width: 24, height: 24)
+                                .overlay {
+                                    Image(systemName: "xmark")
+                                        .resizable()
+                                        .renderingMode(.template)
+                                        .foregroundStyle(Color.black)
+                                        .frame(width: 10, height: 10)
+                                }
+                                .padding(.trailing, 24)
+                        }
+                    }
+                    if viewModel.infos.count < 7 {
+                        ForEach(viewModel.infos) { snackInfo in
+                            SnackBarView(info: snackInfo.info)
+                        }
+                    } else {
+                        ScrollView {
+                            VStack(spacing: 10) {
+                                ForEach(viewModel.infos) { snackInfo in
+                                    SnackBarView(info: snackInfo.info)
+                                }
+                            }
+                        }
+                        .frame(height: UIScreen.main.bounds.width)
+                    }
+                }
+            } else {
+                ForEach(viewModel.recentInfo) { snackInfo in
+                    SnackBarView(info: snackInfo.info)
+                        .offset(x: CGFloat((viewModel.infos.count - 1 - snackInfo.id) * 10),
+                                y: CGFloat((viewModel.infos.count - 1 - snackInfo.id) * -10))
+                        .animation(.easeInOut, value: snackInfo.id)
+                }
+            }
+        }
+        .animation(.easeInOut, value: viewModel.isPresentedDetail)
+        .onTapGesture {
+            viewModel.openToasts()
+        }
+    }
+}
 
 // MARK: - Preview
 
 struct SnackBarPreview: View {
+    
+    @State private var isPresentedSheet = false
         
-    let info = SnackBarInfo(message: "Bonjour je suis un Dracofeu, j'aime les tomates et je crache du feu",
-                            image: nil,
+    let info = SnackBarInfo(message: "J'aime les tomates",
                             backgroundColor: .red,
-                            displayTime: 2)
-    let info2 = SnackBarInfo(message: "Bonjour je suis un Florizarre, j'aime les salade et fouette les gens",
+                            displayTime: 60)
+    let info2 = SnackBarInfo(message: "J'aime les salade",
                              image: .init(systemName: "doc"),
                              backgroundColor: .green)
-    let info3 = SnackBarInfo(message: "Bonjour je suis un Tortank, j'aime les raisins et j'arose!",
-                             image: nil,
-                             backgroundColor: .blue)
+    let info3 = SnackBarInfo(message: "J'aime les raisins!")
     
     var body: some View {
         actionButton
-            .snackBarCenter()
+            .snackBarCenter(position: .bottom, style: .stack)
     }
     
     var actionButton: some View {
         Button(action: {
             NotificationCenter.snackBarNotif(info: [info, info2, info3].randomElement()!)
         }, label: {
-            Text("try me")
+            Text("Send Toast")
         })
     }
 }
