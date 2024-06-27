1561dd976cff4bce54786646680db82d8d8b8321
Luc CASIMIR <casimir.luc@gmail.com>
2024-06-27 23:56:44 +0200

    update unit test

---
+++ Template/CA-JSON.xctemplate/___FILEBASENAME___Preview.swift
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+//  Template: 5.0
+
+// MARK: - Environment
+
+import Foundation
+import LCTool
+
+extension ___VARIABLE_ModuleName:identifier___Response {
+    
+    static var preview: Self {
+        .readPreviewFile(fileName: "example.json")
+    }
+}
