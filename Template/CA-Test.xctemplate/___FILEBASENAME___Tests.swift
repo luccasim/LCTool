e038e2f8d99cd7ee0d4868e10256062b9b016b80
Luc CASIMIR <casimir.luc@gmail.com>
2024-06-28 07:44:29 +0200

    update test unit

--- Template/CA-Test.xctemplate/___FILEBASENAME___Tests.swift
+++ Template/CA-Test.xctemplate/___FILEBASENAME___Tests.swift
@@ -6,22 +6,34 @@ import XCTest
 
 final class ___VARIABLE_ModuleName:identifier___Tests: XCTestCase {
     
+    var mock: ___VARIABLE_ModuleName:identifier___RepositoryProtocol!
     var sut: ___VARIABLE_ModuleName:identifier___Usecase!
             
     override func setUpWithError() throws {
-        
+        mock = nil
+        sut = nil
     }
     
     // MARK: - Assertion
     
     
-    // MARK: - Tests Case
+    // MARK: - Tests
     
-    func testOne() throws {
+    func testExample() async throws {
         // Given
+        mock = Mock___VARIABLE_ModuleName:identifier___Repository()
+        sut = .init(repository: mock)
         
         // When
+        let result = try await sut.dataTaskAsync(dto: .init())
         
         // Then
+        XCTAssert(true)
     }
+}
+
+// MARK: - Mocks
+
+private class Mock___VARIABLE_ModuleName:identifier___Repository: ___VARIABLE_ModuleName:identifier___RepositoryProtocol {
+
 }
